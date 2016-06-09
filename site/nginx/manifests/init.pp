11d46cfa67076d6c4ed5e47ea26b596fe3fbf160
Marco Lenzo <lenzo.marco@gmail.com>
2016-06-09 10:55:59 +0200

    Refactoring nginx repository

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,5 +1,11 @@
 class nginx {
 
+	File {
+		owner => 'root',
+		group => 'root',
+		mode => '0664',
+	}
+
 	package { 'nginx':
 		ensure => 'present',
 	}
@@ -7,43 +13,28 @@ class nginx {
 	file { '/etc/nginx/nginx.conf':
 		ensure => 'file',
 		source => 'puppet:///modules/nginx/nginx.conf',
-		owner => 'root',
-		group => 'root',
-		mode => '0644',
 		require => Package['nginx'],
 		notify  => Service['nginx'],
 	}
 
 	file { '/etc/nginx/conf.d':
 	    ensure => directory,
-	    owner  => 'root',
-	    group  => 'root',
-	    mode   => '0775',
 	}
 
 	file { '/etc/nginx/conf.d/default.conf':
 		ensure => file,
 		source => 'puppet:///modules/nginx/default.conf',
-		owner => 'root',
-		group => 'root',
-		mode => '0644',
 		require => Package['nginx'],
 		notify  => Service['nginx'],
 	}
 
 	file { '/var/www':
 		ensure => directory,
-		owner => 'root',
-		group => 'root',
-		mode => '0775',
 	}
 
 	file { '/var/www/index.html':
 		ensure => file,
 	 	source => 'puppet:///modules/nginx/index.html',
-	 	owner  => 'root',
-	 	group  => 'root',
-	 	mode   => '0664',
 	}
 
 	service { 'nginx':
